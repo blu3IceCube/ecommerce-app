18f1cbfa195405a3f41ec96b362ccd417458cf3e
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-19 22:03:20 +0530

    footer copyright

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -41,7 +41,7 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
               <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
             </Link>
 
-            <p>l</p>
+            <p>{footer.copyright}</p>
           </div>
         </Gutter>
       </div>
