4192aee64c01f5b72bfcf105ce2633f462cf552e
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-19 14:33:50 +0530

    finishing navbar

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -8,7 +8,6 @@ import { usePathname } from 'next/navigation'
 import { Header } from '../../../../payload/payload-types'
 import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
-import MobileNav from '../MobileNav'
 import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
@@ -27,7 +26,6 @@ const HeaderComponent = ({ header }: { header: Header }) => {
         </Link>
 
         <HeaderNav header={header} />
-        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
