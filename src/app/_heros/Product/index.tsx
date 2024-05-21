5107fe72b5b09f41c254424fd6ff30391b040503
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-05-21 17:17:18 +0530

    adding product details page

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import Link from 'next/link'
 
-import { Product } from '../../../payload/payload-types'
+import { Category, Product } from '../../../payload/payload-types'
 import { AddToCartButton } from '../../_components/AddToCartButton'
 import { Gutter } from '../../_components/Gutter'
 import { Media } from '../../_components/Media'
@@ -35,22 +35,19 @@ export const ProductHero: React.FC<{
         <div className={classes.categoryWrapper}>
           <div className={classes.categories}>
             {categories?.map((category, index) => {
-              if (typeof category === 'object' && category !== null) {
-                const { title: categoryTitle } = category
+              const { title: categoryTitle } = category as Category
 
-                const titleToUse = categoryTitle || 'Untitled category'
+              const titleToUse = categoryTitle || 'Untitled category'
 
-                const isLast = index === categories.length - 1
+              const isLast = index === categories.length - 1
 
-                return (
-                  <p key={index} className={classes.category}>
-                    {titleToUse}
-                    {!isLast && <Fragment>, &nbsp;</Fragment>}
-                    <span className={classes.separator}>|</span>
-                  </p>
-                )
-              }
-              return null
+              return (
+                <p key={index} className={classes.category}>
+                  {titleToUse}
+                  {!isLast && <Fragment>, &nbsp;</Fragment>}
+                  <span className={classes.separator}>|</span>
+                </p>
+              )
             })}
           </div>
           <p className={classes.stock}> In stock</p>
